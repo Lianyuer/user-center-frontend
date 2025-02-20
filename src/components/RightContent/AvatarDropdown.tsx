6ea34a4df66a50b01b494940af1d8236ede643ab
lianyu <1319764245@qq.com>
2025-02-20 12:55:29 +0800

    feat(UserManage): add user logout

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,4 +1,4 @@
-import { outLogin } from '@/services/ant-design-pro/api';
+import { logout } from '@/services/ant-design-pro/api';
 import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Menu, Spin } from 'antd';
 import type { ItemType } from 'antd/lib/menu/hooks/useItems';
@@ -17,7 +17,7 @@ export type GlobalHeaderRightProps = {
  * 退出登录，并且将当前的 url 保存
  */
 const loginOut = async () => {
-  await outLogin();
+  await logout();
   const { query = {}, search, pathname } = history.location;
   const { redirect } = query;
   // Note: There may be security issues, please note
@@ -33,7 +33,6 @@ const loginOut = async () => {
 
 const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
   const { initialState, setInitialState } = useModel('@@initialState');
-
   const onMenuClick = useCallback(
     (event: MenuInfo) => {
       const { key } = event;
@@ -46,7 +45,6 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
     },
     [setInitialState],
   );
-
   const loading = (
     <span className={`${styles.action} ${styles.account}`}>
       <Spin
@@ -101,7 +99,12 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
   return (
     <HeaderDropdown overlay={menuHeaderDropdown}>
       <span className={`${styles.action} ${styles.account}`}>
-        <Avatar size="small" className={styles.avatar} src={currentUser.avatarUrl} alt="avatarUrl" />
+        <Avatar
+          size="small"
+          className={styles.avatar}
+          src={currentUser.avatarUrl}
+          alt="avatarUrl"
+        />
         <span className={`${styles.name} anticon`}>{currentUser.nickName}</span>
       </span>
     </HeaderDropdown>
